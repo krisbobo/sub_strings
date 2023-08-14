e0b10537b15e3359b7983bfcfe0267a6db572e9f
krisbobo <kiyongbohbo@gmail.com>
2023-08-14 23:50:33 +0100

    Add method

---
+++ challenge.rb
@@ -0,0 +1,8 @@
+def substrings(str, dic)
+
+end
+
+# > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# > substrings("below", dictionary)
+# => { "below" => 1, "low" => 1 }
