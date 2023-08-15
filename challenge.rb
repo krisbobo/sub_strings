a40bdadcd343d81928113d5de624796632812151
krisbobo <kiyongbohbo@gmail.com>
2023-08-15 23:34:12 +0100

    Add final working method

--- challenge.rb
+++ challenge.rb
@@ -1,8 +1,12 @@
 def substrings(str, dic)
-
+  str = str.downcase
+  dic.reduce(Hash.new(0)) do |acc, word|
+    acc[word] += 1 if str.include?(word)
+    acc
+  end
 end
 
-# > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 # => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# > substrings("below", dictionary)
+puts substrings("below", dictionary)
 # => { "below" => 1, "low" => 1 }
